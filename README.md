c803495eaaf1150ecaed89186182b976df8728b1
sleshatuladhar <sleshatuladhar@gmail.com>
2023-09-18 09:49:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# STuladhar-PROG5001-A2
+ Assignment2 for PROG5001
---
+++ Student.java
@@ -0,0 +1,17 @@
+
+/**
+ * Student class
+ *
+ * @author (Slesha)
+ * @version (18/09/2023)
+ */
+public class Student
+{
+    // Define instance variables
+    String lastName;
+    String firstName;
+    int studentId;
+    double a1Mark;
+    double a2Mark;
+    double a3Mark;
+}
