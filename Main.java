eb8afa6b1680d0a97de84e6b3b2b50d631b23f2d
sleshatuladhar <sleshatuladhar@gmail.com>
2023-09-18 09:55:09 +1000

    Initialise main class

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Main class
+ *
+ * @author (Slesha Tuladhar)
+ * @version (18/09/2023)
+ */
+public class Main
+{
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+    }
+}
