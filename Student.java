ed93036819cf8a1f5819a41145ca28b3e22ce2ca
sleshatuladhar <sleshatuladhar@gmail.com>
2023-09-25 10:35:15 +1000

    Calculate total marks

--- Student.java
+++ Student.java
@@ -14,6 +14,7 @@ public class Student
     double a1Mark;
     double a2Mark;
     double a3Mark;
+    double totalMarks;
     
     //Define constructor
     public Student(String lastName, String firstName, int studentId, double a1Mark, double a2Mark, double a3Mark){
@@ -23,5 +24,14 @@ public class Student
         this.a1Mark = a1Mark;
         this.a2Mark = a2Mark;
         this.a3Mark = a3Mark;
+        this.calculateTotalMarks();
+    }
+    
+    public void calculateTotalMarks(){
+        this.totalMarks = this.a1Mark + this.a2Mark + this.a3Mark;
+    }
+    
+    public double getTotalMarks(){
+        return this.totalMarks;
     }
 }
