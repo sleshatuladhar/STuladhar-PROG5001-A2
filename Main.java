c4c8256433ceda7e1f7d34d5d2c1562c43ea329e
sleshatuladhar <sleshatuladhar@gmail.com>
2023-10-01 23:48:16 +1100

    Remove constructor

--- Main.java
+++ Main.java
@@ -9,13 +9,6 @@ import java.io.*;
  */
 public class Main
 {
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-    }
-    
     public static void main(String[] args){
         //Initialise variables
         Scanner scanner = new Scanner(System.in);
